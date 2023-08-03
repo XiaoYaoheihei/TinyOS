9fcb074d5ec701f33cd5714ff7c237edac45a3ab
XiaoYaoheihei <1550949541@qq.com>
2023-08-03 20:39:34 +0800

    main函数

--- c3/kernel/main.c
+++ c3/kernel/main.c
@@ -1,7 +1,19 @@
-int main() {
+// #include "print.h"
+void main() {
+  // put_char('k');
+  // put_char('e');
+  // put_char('r');
+  // put_char('n');
+  // put_char('e');
+  // put_char('l');
+  // put_char('\n');
+  // put_char('1');
+  // put_char('2');
+  // put_char('\b');
+  // put_char('3');
   while(1);
-  return 0;
 }
+
 //ld main.o -Ttext 0xc0001500 -e main -o kernel.bin
 //显示使用-e指明可执行文件的程序入口是main
 //因为一般在链接完成之后会有_start的默认地址，他是默认的程序入口
