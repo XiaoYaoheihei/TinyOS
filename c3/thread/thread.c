a3551cf7f22747d6711d7dcebd2d68dc6f84bec0
XiaoYaoheihei <1550949541@qq.com>
2023-08-09 20:30:08 +0800

    kernel下线程的实现

---
+++ c3/thread/thread.c
@@ -0,0 +1,56 @@
+#include "thread.h"
+#include "../lib/stdint.h"
+#include "../lib/string.h"
+#include "../kernel/global.h"
+#include "../kernel/memory.h"
+
+#define PG_SIZE 4096
+
+//由kernel_thread去执行function(func_argc)
+static void kernel_thread(thread_func* function, void* func_argc) {
+  function(func_argc);
+}
+
+//初始化线程栈
+//将待执行的函数和参数放到线程栈中的相应位置
+void thread_create(struct task_struct* pthread, thread_func function, void* func_argc) {
+  //预留好了中断栈
+  pthread->self_kstack -= sizeof(struct intr_stack);
+  //再预留线程栈
+  pthread->self_kstack -= sizeof(struct thread_stack);
+  struct thread_stack* kthread_stack = (struct thread_stack*)pthread->self_kstack;
+  kthread_stack->eip = kernel_thread;
+  kthread_stack->function = function;
+  kthread_stack->func_arg = func_argc;
+  kthread_stack->ebp = kthread_stack->ebx = \
+  kthread_stack->esi = kthread_stack->edi = 0;
+}
+
+//初始化线程对应的PCB基本信息
+void init_thread(struct task_struct* pthread, char* name, int prio) {
+  memset(pthread, 0, sizeof(*pthread));
+  strcpy(pthread->name, name);
+  pthread->status = TASK_RUNNING;
+  pthread->priority = prio;
+  //self_kstack是线程在自己的内核态下使用的栈顶地址
+  //因为刚好PCB分配的是1页，所以指向的是这1页的顶端
+  pthread->self_kstack = (uint32_t*)((uint32_t)pthread+PG_SIZE);
+  //自定义的魔数
+  pthread->stack_magic = 0x19870916;
+}
+
+//创建一个优先级为prio的线程
+//线程名字为name,执行的函数是function(func_arg)
+struct task_struct* thread_start(char* name,
+                                 int prio,
+                                 thread_func* function,
+                                 void* func_arg) {
+  //首先给PCB分配一个页空间,PCB位于内核空间中
+  struct task_struct* thread = get_kernel_pages(1);
+  //初始化PCB
+  init_thread(thread, name, prio);
+  thread_create(thread, function, func_arg);
+
+
+  return thread;
+}
---
+++ c3/thread/thread.h
@@ -0,0 +1,80 @@
+#ifndef _THREAD_THREAD_H
+#define _THREAD_THREAD_H
+
+#include "../lib/stdint.h"
+
+// 自定义的通用函数类型
+typedef void thread_func(void*);
+
+// 进程或者线程的状态
+enum task_status{
+  TASK_RUNNING,
+  TASK_READY,
+  TASK_BLOCKED,
+  TASK_WAITING,
+  TASK_HANDING,
+  TASK_DIED
+};
+
+// 中断栈
+// 此结构用于中断发生时保护程序的上下文
+// 进入中断之后，在中断入口程序所执行的上下文保护的一系列压栈操作都是压入此结构中
+// 会按照此结构压入上下文寄存器
+// 此栈在线程自己的内核栈中位置固定，所在页的最顶端
+struct intr_stack{
+  // kernel.asm 宏 VECTOR 中 push %1 压入的中断号
+  uint32_t vector_number;
+  uint32_t edi;
+  uint32_t esi;
+  uint32_t ebp;
+  uint32_t esp_dummy;
+  // 虽然 pushad 把 esp 也压入，但 esp 是不断变化的，所以会被 popad 忽略
+  uint32_t ebx;
+  uint32_t edx;
+  uint32_t ecx;
+  uint32_t eax;
+  uint32_t gs;
+  uint32_t fs;
+  uint32_t es;
+  uint32_t ds;
+  // CPU从低特权级进入高特权级时才会压入
+  // err_code 会被压入在 eip 之后
+  uint32_t err_code;
+  void (*eip) (void);
+  uint32_t cs;
+  void* esp;
+  uint32_t ss;
+};
+
+// 线程栈
+// 线程自己的栈，用于存储线程中待执行的函数
+// 此结构在线程自己的内核栈中位置不固定
+struct thread_stack{
+  uint32_t ebp;
+  uint32_t ebx;
+  uint32_t edi;
+  uint32_t esi;
+  //线程首次执行函数的时候，eip就是待调用的函数
+  //其他时候，当任务切换的时候，此eip保存的是任务切换后的新任务的返回地址
+  void (*eip) (thread_func* func, void* func_arg);
+
+  //以下仅供第一次被调度上CPU时使用
+  void (*unused_retaddr);   //参数 unused_ret 只为占位置充数为返回地址
+  thread_func* function;    //线程中执行的函数
+  void* func_arg;           //执行函数需要使用的参数
+};
+
+//进程或者线程的程序控制块（PCB）
+//PCB和0级栈是在同一个页内的
+struct task_struct {
+  //各个内核线程自己的内核栈
+  uint32_t* self_kstack;
+  enum task_status status;
+  //优先级
+  uint8_t priority;
+  char name[16];
+  //栈的边界标记，用于检测栈的溢出
+  uint32_t stack_magic;
+};
+
+#endif
