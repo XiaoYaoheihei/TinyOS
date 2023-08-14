1b54bd3ee14b05768ea0386d4eef62cd0c20111a
XiaoYaoheihei <1550949541@qq.com>
2023-08-14 20:40:14 +0800

    键盘中断处理程序

---
+++ c3/device/keyboard.h
@@ -0,0 +1,9 @@
+#ifndef __DEVICE_KEYBOARD_H
+#define __DEVICE_KEYBOARD_H
+#include "../lib/kernel/print.h"
+#include "../kernel/interrupt.h"
+#include "../lib/kernel/io.h"
+#include "../kernel/global.h"
+
+void keyboard_init(); 
+#endif
