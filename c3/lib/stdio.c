f2e5e998c43cce85c7688fb9b03610586b220877
XiaoYaoheihei <1550949541@qq.com>
2023-08-21 19:39:22 +0800

    printf实现%c,d,s的功能

--- c3/lib/stdio.c
+++ c3/lib/stdio.c
@@ -5,6 +5,7 @@
 //把 ap 指向第一个固定参数 v的地址
 #define va_start(ap, v) ap = (va_list)&v 
 //ap 指向下一个参数并返回其值，栈中的参数占4字节，所以+4
+//压入栈中的参数是字符串的地址
 #define va_arg(ap, t) *((t*)(ap += 4))
 //清除 ap
 #define va_end(ap) ap = NULL
@@ -22,6 +23,8 @@ static void itoa(uint32_t value, char** buf_ptr_addr, uint8_t base) {
   // 如果余数是 0～9
   if (m < 10) {
     //将数字 0～9 转换为字符'0'～'9'
+    //*buf_ptr_addr得到临时变量中的值，是个指针
+    //进行++操作，然后再*可以得到该指针指向的值
     *((*buf_ptr_addr)++) = m + '0';
   } else {
     //将数字 A～F 转换为字符'A'～'F'
@@ -35,7 +38,10 @@ uint32_t vsprintf(char* str, const char* format, va_list ap) {
   char* buf_ptr = str;
   const char* index_ptr = format;
   char index_char = *index_ptr;
+  //用来处理整数
   int32_t arg_int;
+  //用来处理字符串
+  char* arg_str;
   while (index_char) {
     if (index_char != '%') {
       *(buf_ptr++) = index_char;
@@ -45,17 +51,51 @@ uint32_t vsprintf(char* str, const char* format, va_list ap) {
     //得到%后面的字符
     index_char = *(++index_ptr);
     switch (index_char) {
+    case 's'://%s
+      arg_str = va_arg(ap, char*);
+      strcpy(buf_ptr, arg_str);
+      buf_ptr += strlen(arg_str);
+      index_char = *(++index_ptr);
+      break;
+
+    case 'c'://%c
+      *(buf_ptr++) = va_arg(ap, char);
+      index_char = *(++index_ptr);
+      break;
+
+    case 'd'://%d
+      arg_int = va_arg(ap, int);
+      //若是负数，将其转为正数后，在正数前面输出个负号'-'
+      if (arg_int < 0) {
+        arg_int = 0-arg_int;
+        *buf_ptr++ = '-';
+      }
+      itoa(arg_int, &buf_ptr, 10);
+      index_char = *(++index_ptr);
+      break;
+
     case 'x':
+      //具体的值arg_int
       arg_int  = va_arg(ap, int);
       itoa(arg_int, &buf_ptr, 16);
       index_char = *(++index_ptr);
       //跳过格式字符并更新 index_char
       break;
-    
     }
   }
   return strlen(str);
 }
+
+//同 printf 不同的地方就是字符串不是写到终端，而是写到 buf 中
+uint32_t sprintf(char* buf, const char* format, ...) {
+  va_list args;
+  uint32_t retval;
+  va_start(args, format);
+  retval = vsprintf(buf, format, args);
+  va_end(args);
+  return retval;
+}
+
 
 //格式化输出字符串format
 uint32_t printf(const char* format, ...) {
