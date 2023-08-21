4d090d85dd83c1bf7e29bc8938f3e75b9f7bbe10
XiaoYaoheihei <1550949541@qq.com>
2023-08-21 12:01:15 +0800

    添加write系统调用

--- c3/userprog/syscall_init.c
+++ c3/userprog/syscall_init.c
@@ -1,11 +1,20 @@
 #include "syscall_init.h"
 #include "../lib/user/syscall.h"
+#include "../device/console.h"
+#include "../lib/string.h"
 #include "print.h"
 #include "thread.h"
 
 #define syscall_nr 32
 typedef void* syscall;
 syscall syscall_table[syscall_nr];
+
+
+//打印字符串str，未实现文件系统前的版本
+uint32_t sys_write(char* str) {
+  console_put_str(str);
+  return strlen(str);
+}
 
 uint32_t sys_getpid(void) {
   return running_thread()->pid;  
@@ -14,5 +23,6 @@ uint32_t sys_getpid(void) {
 void syscall_init(void) {
   put_str("syscall_init start\n");
   syscall_table[SYS_GETPID] = sys_getpid;
+  syscall_table[SYS_WRITE] = sys_write;
   put_str("syscall_init done\n");
 }
