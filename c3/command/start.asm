eafa5bfe66f0a1433e4d6c8c10834230c9b8375f
XiaoYaoheihei <1550949541@qq.com>
2023-09-06 21:27:46 +0800

    支持cat命令

--- c3/command/start.asm
+++ c3/command/start.asm
@@ -1,5 +1,6 @@
 [bits 32]
 extern main
+extern exit 
 section .text
 global _start
 _start:
@@ -9,3 +10,8 @@ _start:
   ;压入 argc
   push ecx 
   call main
+
+  ;将 main 的返回值通过栈传给 exit，gcc 用 eax 存储返回值，这是 ABI 规定的
+  push eax
+  call exit 
+  ;exit 不会返回
